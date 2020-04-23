640379b6725c3514914a02a092bcb18a8627dc7c
Jinjin Wang <jinjinwang@MacBook-Pro.lan>
2020-04-23 16:23:39 +0100

    add .env

--- routes/notes.js
+++ routes/notes.js
@@ -21,28 +21,9 @@ router.route('/add').post((req, res) => {
       .catch(err => res.status(400).json('Error: ' +err));
 });
 
-router.route('/:id').get((req, res) => {
-    Note.findById(req.params.id)
-    .then(note => res.json(note))
-    .catch(err => res.status(400).json('Error: ' +err));
-});
-
 router.route('/:id').delete((req, res) => {
     Note.findByIdAndDelete(req.params.id)
     .then(() => res.json('Note deleted.'))
-    .catch(err => res.status(400).json('Error: ' +err));
-});
-
-router.route('/update/:id').post((req, res) => {
-    Note.findById(req.params.id)
-    .then((note) => {
-    note.title = req.body.title;
-    note.content = req.body.content;
-
-    note.save()
-    .then(() => res.json('Note updated!'))
-    .catch(err => res.status(400).json('Error: ' +err));
-    })
     .catch(err => res.status(400).json('Error: ' +err));
 });
 
