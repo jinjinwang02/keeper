3e53341e604bcc1d7d2035eb12ff86b9362db606
Jinjin Wang <jinjinwang@MacBook-Pro.lan>
2020-04-23 21:57:20 +0100

    change domain name

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ function App() {
 const [posts, setPosts] = useState([]);
 
 useEffect(() => {
-  axios.get('http://localhost:5000/notes')
+  axios.get('https://calm-tor-86699.herokuapp.com/notes')
     .then(res => {
       console.log(res)
       setPosts(res.data)
