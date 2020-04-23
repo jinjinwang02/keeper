47287b57bb705c161a6524dd169dda68b48fb34c
Jinjin Wang <jinjinwang@MacBook-Pro.lan>
2020-04-23 12:26:16 +0100

    delete port

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 
 const app = express();
 
-// var corsOptions = {
-//   origin: 'http://localhost:3000',
-// }
+var corsOptions = {
+  origin: 'http://localhost:3000',
+}
 
 app.use(cors());
 app.use(express.json());
@@ -38,7 +38,7 @@ if(process.env.NODE_DEV === 'production'){
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server is running on port : ${port}`));
   
