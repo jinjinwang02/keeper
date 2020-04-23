1f9a18f8c09ee28eee55f54130dfdc04aeec9141
Jinjin Wang <jinjinwang@MacBook-Pro.lan>
2020-04-23 21:24:08 +0100

    deploy to heroku with proxy

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
-var corsOptions = {
-  origin: 'http://localhost:3000',
-}
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+// }
 
 app.use(cors());
 app.use(express.json());
@@ -38,6 +36,8 @@ if(process.env.NODE_ENV === 'production'){
     res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
   });
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port : ${port}`));
   
