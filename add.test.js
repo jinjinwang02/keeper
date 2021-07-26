83d71f2b03a1453a28606998f150e69486353f76
Jinjin Wang <jinjinwang02@gmail.com>
2021-07-26 18:29:25 +0100

    feat: add portfolio website

--- add.test.js
+++
@@ -1,8 +0,0 @@
-const add = require('./add');
-
-describe('test', () => {
-  it('adds', () => {
-    const result = add(1, 2);
-    expect(result).toEqual(3);
-  });
-});
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,28 +1,71 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Header() {
   const [isExpanded, setExpanded] = useState(false);
 
   function expand() {
-    setExpanded(preValue => {
-      return !preValue
+    setExpanded((preValue) => {
+      return !preValue;
     });
   }
 
   return (
     <header>
-      <h1 className="page-title">Keeper</h1>
-      <button onClick={expand} className="about-button">ABOUT</button>
+      <h1 className='page-title'>Keeper</h1>
+      <button onClick={expand} className='about-button'>
+        ABOUT
+      </button>
       {isExpanded && (
         <div>
           <br></br>
-          <p>This minimalist single-page application allows you to keep and delete notes.</p>
+          <p>
+            This minimalist single-page application allows you to keep and
+            delete notes.
+          </p>
           <br></br>
-          <p>I'm Jinjin Wang, a front-end developer and multidisciplinary fashion designer based in London.</p>
-          <p>Contact me by: </p>
-          <i className="far fa-envelope"></i> <a href="mailto: jinjinwang@gmail.com" target="_blank" rel="noopener noreferrer">Email   &nbsp;&nbsp;</a>
-          <i className="icon fab fa-linkedin"> <a href="https://www.linkedin.com/in/jinjin-wang-616a99183/" target="_blank" rel="noopener noreferrer">Linkedin &nbsp;</a></i>
-          <i className="fab fa-github"></i> <a href="https://github.com/jinjinwang02" target="_blank" rel="noopener noreferrer">Github</a>
+          <p>
+            I'm Jinjin Wang, a Front End Developer and Multidisciplinary
+            Designer based in London.
+          </p>
+          <p>You can find me via:</p>
+          <i className='fas fa-laptop'></i>{' '}
+          <a
+            href='https://jinjinwang.co.uk'
+            target='_blank'
+            rel='noopener noreferrer'
+            style={{ marginRight: '16px' }}
+          >
+            Portfolio website
+          </a>
+          <i className='far fa-envelope'></i>{' '}
+          <a
+            href='mailto: jinjinwang@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            style={{ marginRight: '16px' }}
+          >
+            Email
+          </a>
+          <i className='icon fab fa-linkedin'>
+            {' '}
+            <a
+              href='https://www.linkedin.com/in/jinjin-wang/'
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ marginRight: '16px' }}
+            >
+              Linkedin
+            </a>
+          </i>
+          <i className='fab fa-github'></i>{' '}
+          <a
+            href='https://github.com/jinjinwang02'
+            target='_blank'
+            rel='noopener noreferrer'
+            style={{ marginRight: '16px' }}
+          >
+            Github
+          </a>
         </div>
       )}
     </header>
